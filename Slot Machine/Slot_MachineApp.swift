b4b261911d6bfff39265f9c131580ce44dcc3144
Htet Aung Shine <103210228+HtetAungShine6@users.noreply.github.com>
2023-06-27 21:42:57 +0700

    initial commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Akito Daiki on 7/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
